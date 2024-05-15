9fd5c10505b8ba0cd426baf90781dd2fb2340658
bondroid1322 <bondroid3.22@icloud.com>
2024-05-15 17:46:31 +0900

    add: フィボナッチ数列アルゴリズムの作成

--- fibo.py
+++ fibo.py
@@ -1,7 +1,16 @@
 import sys
 
 def fibo(n):
-    return n
+
+    memo = [-1 for _ in range(n)]
+    memo[0] = 1
+    memo[1] = 1
+
+    for i in range(2, n):
+        if (memo[i] == -1):
+            memo[i] = memo[i-1] + memo[i-2]
+
+    return memo[n-1]
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
